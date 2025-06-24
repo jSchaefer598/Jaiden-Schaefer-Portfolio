f1a14173b15d6f93ae8fc4649bab1cf2824c8756
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-24 10:22:38 -0500

    Dark mode

--- brains.js
+++ brains.js
@@ -1,3 +1,44 @@
 let darkMode = document.querySelector(".dark-mode");
 let lightMode = document.querySelector(".light-mode");
 
+let header = document.querySelector(".header");
+let headerText = document.querySelector("h1");
+let header2 = document.querySelector("h2");
+let content = document.querySelector(".content");
+let header3 = document.querySelector("h3");
+let paragraph = document.querySelector("p");
+
+let contact = document.querySelector("#contact");
+let resume = document.querySelector("#resume");
+let projects = document.querySelector("#projects");
+let aboutMe = document.querySelector("#about-me");
+
+darkMode.addEventListener("click", function(){
+    header.style.backgroundColor = "#d5f1ef";
+    headerText.style.color = "#18455d";
+    header2.style.color = "#d5f1ef";
+    content.style.backgroundColor = "#18455d"
+    darkMode.style.display = "none";
+    lightMode.style.display = "block";
+    contact.src = "phoneDark.png";
+    resume.src = "resumeDark.png";
+    projects.src = "projectsDark.png";
+    aboutMe.src = "aboutMeDark.png";
+    header3.style.color = "#d5f1ef";
+    paragraph.style.color = "#d5f1ef";
+});
+
+lightMode.addEventListener("click", function(){
+    header.style.backgroundColor = "#18455d";
+    headerText.style.color = "#d5f1ef";
+    header2.style.color = "#18455d";
+    content.style.backgroundColor = "#d5f1ef";
+    darkMode.style.display = "block";
+    lightMode.style.display = "none";
+    contact.src = "phone.png";
+    resume.src = "resume.png";
+    projects.src = "projects.png";
+    aboutMe.src = "aboutMe.png";
+    header3.style.color = "#18455d";
+    paragraph.style.color = "#18455d";
+});
