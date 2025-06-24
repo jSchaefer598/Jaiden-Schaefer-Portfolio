5f9ca533154cb09d6fbc155105cfea48e3431796
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-24 14:53:10 -0500

    Light and dark

--- brains.js
+++ brains.js
@@ -13,32 +13,77 @@ let resume = document.querySelector("#resume");
 let projects = document.querySelector("#projects");
 let aboutMe = document.querySelector("#about-me");
 
-darkMode.addEventListener("click", function(){
-    header.style.backgroundColor = "#d5f1ef";
-    headerText.style.color = "#18455d";
+
+// Check in localstorage is dark mode is enabled
+let isDarkModeEnabled = localStorage.getItem("isDarkModeEnabled");
+console.log(isDarkModeEnabled);
+
+
+if(isDarkModeEnabled === "true") {
+    console.log("set css to dark mode");
+    // dark mode CSS
+    header.style.backgroundColor = "#18455d";
+    headerText.style.color = "#d5f1ef";
     header2.style.color = "#d5f1ef";
     content.style.backgroundColor = "#18455d"
     darkMode.style.display = "none";
     lightMode.style.display = "block";
+    contact.src = "phone.png";
+    resume.src = "resume.png";
+    projects.src = "projects.png";
+    aboutMe.src = "aboutMe.png";
+    // header3.style.color = "#d5f1ef";
+    // paragraph.style.color = "#d5f1ef";
+} else {
+    console.log("set css to light mode");
+    // javascript for light mode CSS
+    header.style.backgroundColor = "#d5f1ef";
+    headerText.style.color = "#18455d";
+    header2.style.color = "#18455d";
+    content.style.backgroundColor = "#d5f1ef";
+    darkMode.style.display = "block";
+    lightMode.style.display = "none";
     contact.src = "phoneDark.png";
     resume.src = "resumeDark.png";
     projects.src = "projectsDark.png";
     aboutMe.src = "aboutMeDark.png";
+    // header3.style.color = "#18455d";
+    // paragraph.style.color = "#18455d";
+}
+
+darkMode.addEventListener("click", function(){
+    localStorage.setItem("isDarkModeEnabled", "true");
+    
+    
+    header.style.backgroundColor = "#18455d";
+    headerText.style.color = "#d5f1ef";
+    header2.style.color = "#d5f1ef";
+    content.style.backgroundColor = "#18455d"
+    darkMode.style.display = "none";
+    lightMode.style.display = "block";
+    contact.src = "phone.png";
+    resume.src = "resume.png";
+    projects.src = "projects.png";
+    aboutMe.src = "aboutMe.png";
     header3.style.color = "#d5f1ef";
     paragraph.style.color = "#d5f1ef";
 });
 
 lightMode.addEventListener("click", function(){
-    header.style.backgroundColor = "#18455d";
-    headerText.style.color = "#d5f1ef";
+    localStorage.setItem("isDarkModeEnabled", "false");
+
+    header.style.backgroundColor = "#d5f1ef";
+    headerText.style.color = "#18455d";
     header2.style.color = "#18455d";
     content.style.backgroundColor = "#d5f1ef";
     darkMode.style.display = "block";
     lightMode.style.display = "none";
-    contact.src = "phone.png";
-    resume.src = "resume.png";
-    projects.src = "projects.png";
-    aboutMe.src = "aboutMe.png";
+    contact.src = "phoneDark.png";
+    resume.src = "resumeDark.png";
+    projects.src = "projectsDark.png";
+    aboutMe.src = "aboutMeDark.png";
     header3.style.color = "#18455d";
     paragraph.style.color = "#18455d";
 });
+
+
