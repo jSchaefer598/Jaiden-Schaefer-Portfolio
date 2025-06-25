03cabd02c8f836396cfb279a14672b0cc32a770d
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-25 10:00:19 -0500

    Light and dark

--- brains.js
+++ brains.js
@@ -5,13 +5,15 @@ let header = document.querySelector(".header");
 let headerText = document.querySelector("h1");
 let header2 = document.querySelector("h2");
 let content = document.querySelector(".content");
-let header3 = document.querySelector("h3");
 let paragraph = document.querySelector("p");
 
 let contact = document.querySelector("#contact");
 let resume = document.querySelector("#resume");
 let projects = document.querySelector("#projects");
 let aboutMe = document.querySelector("#about-me");
+let email = document.querySelector("#email");
+let call = document.querySelector("#call");
+let github = document.querySelector("#github");
 
 
 // Check in localstorage is dark mode is enabled
@@ -22,68 +24,73 @@ console.log(isDarkModeEnabled);
 if(isDarkModeEnabled === "true") {
     console.log("set css to dark mode");
     // dark mode CSS
-    header.style.backgroundColor = "#18455d";
-    headerText.style.color = "#d5f1ef";
-    header2.style.color = "#d5f1ef";
-    content.style.backgroundColor = "#18455d"
     darkMode.style.display = "none";
     lightMode.style.display = "block";
+    header.style.backgroundColor = "#18455d";
+    headerText.style.color = "#d5f1ef";
     contact.src = "phone.png";
     resume.src = "resume.png";
     projects.src = "projects.png";
     aboutMe.src = "aboutMe.png";
-    // header3.style.color = "#d5f1ef";
-    // paragraph.style.color = "#d5f1ef";
+    header2.style.color = "#d5f1ef";
+    content.style.backgroundColor = "#18455d";
+    content.style.color= "#d5f1ef";
+    paragraph.style.color = "#d5f1ef";
 } else {
     console.log("set css to light mode");
     // javascript for light mode CSS
-    header.style.backgroundColor = "#d5f1ef";
-    headerText.style.color = "#18455d";
-    header2.style.color = "#18455d";
-    content.style.backgroundColor = "#d5f1ef";
     darkMode.style.display = "block";
     lightMode.style.display = "none";
+    header.style.backgroundColor = "#d5f1ef";
+    headerText.style.color = "#18455d";
     contact.src = "phoneDark.png";
     resume.src = "resumeDark.png";
     projects.src = "projectsDark.png";
     aboutMe.src = "aboutMeDark.png";
-    // header3.style.color = "#18455d";
-    // paragraph.style.color = "#18455d";
+    header2.style.color = "#18455d";
+    content.style.backgroundColor = "#d5f1ef";
+    content.style.color = "#18455d";
+    paragraph.style.color = "#18455d";
 }
 
 darkMode.addEventListener("click", function(){
     localStorage.setItem("isDarkModeEnabled", "true");
     
-    
-    header.style.backgroundColor = "#18455d";
-    headerText.style.color = "#d5f1ef";
-    header2.style.color = "#d5f1ef";
-    content.style.backgroundColor = "#18455d"
     darkMode.style.display = "none";
     lightMode.style.display = "block";
+    header.style.backgroundColor = "#18455d";
+    headerText.style.color = "#d5f1ef";
     contact.src = "phone.png";
     resume.src = "resume.png";
     projects.src = "projects.png";
     aboutMe.src = "aboutMe.png";
-    header3.style.color = "#d5f1ef";
+    header2.style.color = "#d5f1ef";
+    content.style.backgroundColor = "#18455d";
+    content.style.color = "#d5f1ef";
     paragraph.style.color = "#d5f1ef";
+    email.src = "emailDark.png";
+    call.src = "callDark.png";
+    github.src = "githubDark.png";
 });
 
 lightMode.addEventListener("click", function(){
     localStorage.setItem("isDarkModeEnabled", "false");
 
-    header.style.backgroundColor = "#d5f1ef";
-    headerText.style.color = "#18455d";
-    header2.style.color = "#18455d";
-    content.style.backgroundColor = "#d5f1ef";
     darkMode.style.display = "block";
     lightMode.style.display = "none";
+    header.style.backgroundColor = "#d5f1ef";
+    headerText.style.color = "#18455d";
     contact.src = "phoneDark.png";
     resume.src = "resumeDark.png";
     projects.src = "projectsDark.png";
     aboutMe.src = "aboutMeDark.png";
-    header3.style.color = "#18455d";
+    header2.style.color = "#18455d";
+    content.style.backgroundColor = "#d5f1ef";
+    content.style.color = "#18455d";
     paragraph.style.color = "#18455d";
+    email.src = "email.png";
+    call.src = "call.png";
+    github.src = "github.png";
 });
 
 
